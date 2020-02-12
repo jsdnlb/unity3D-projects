3100b203c76c26cc8585778c14d7b522b7493307
jsdnlb <danielb.alzate1@gmail.com>
2020-02-11 19:52:09 -0500

    FInaliza primer versión, funciona, pero no compila

---
+++ My-Shooter/Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20.0f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        Destroy(gameObject, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(transform.forward * speed);
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        Destroy(gameObject);
+    }
+}
---
+++ My-Shooter/Assets/Scripts/Shooter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject bullet;
+    public float fireRate;
+    private float nextFire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) && Time.time > nextFire) {
+            nextFire = Time.time + fireRate;
+            Instantiate(bullet, gameObject.transform.position, gameObject.transform.rotation);
+        }
+    }
+}
